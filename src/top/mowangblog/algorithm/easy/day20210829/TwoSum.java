ad3ee131cc835a3229e174c1018a665cb85b56c4
mowangblog@qq.com <mowangblog@qq.com>
2021-09-01 10:40:47 +0800

    ✨update

--- src/top/mowangblog/algorithm/easy/day20210829/TwoSum.java
+++ src/top/mowangblog/algorithm/easy/day20210829/TwoSum.java
@@ -1,35 +1,38 @@
 package top.mowangblog.algorithm.easy.day20210829;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 给定一个整数数组 nums和一个整数目标值 target，请你在该数组中找出 和为目标值 target的那两个整数，并返回它们的数组下标。
- *
+ * <p>
  * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
- *
+ * <p>
  * 你可以按任意顺序返回答案。
- *
+ * <p>
  * 示例 1：
- *
+ * <p>
  * 输入：nums = [2,7,11,15], target = 9
  * 输出：[0,1]
  * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
  * 示例 2：
- *
+ * <p>
  * 输入：nums = [3,2,4], target = 6
  * 输出：[1,2]
  * 示例 3：
- *
+ * <p>
  * 输入：nums = [3,3], target = 6
  * 输出：[0,1]
- *
+ * <p>
  * 提示：
- *
+ * <p>
  * 2 <= nums.length <= 104
  * -109 <= nums[i] <= 109
  * -109 <= target <= 109
  * 只会存在一个有效答案
  * 进阶：你可以想出一个时间复杂度小于 O(n2) 的算法吗？
- *
- *
+ * <p>
+ * <p>
  * Algorithm-Daily-practice
  * twoSum
  *
@@ -40,40 +43,36 @@ package top.mowangblog.algorithm.easy.day20210829;
 public class TwoSum {
 
     public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
-        //最简单的方法循环遍历复杂度较高
+//        //最简单的方法循环遍历复杂度较高
+//        int[] result = new int[2];
+//        for (int i = 0; i < nums.length; i++) {
+//            for (int j = i + 1; j < nums.length; j++) {
+//                if (nums[i] + nums[j] == target) {
+//                    result[0] = i;
+//                    result[1] = j;
+//                    return result;
+//                }
+//            }
+//        }
+//        return null;
+        //进阶哈希map
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i+1; j < nums.length; j++) {
-                if(nums[i]+nums[j]==target){
-                    result[0] = i;
-                    result[1] = j;
-                    return result;
-                }
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            } else {
+                map.put(nums[i], i);
             }
         }
         return null;
     }
+
     //实际提交时不用main方法，用来测试
     public static void main(String[] args) {
         TwoSum solution = new TwoSum();
-        int[] nums= new int[]{2, 7, 11, 15};
+        int[] nums = new int[]{2, 7, 11, 15};
         for (int i : solution.twoSum(nums, 9)) {
             System.out.println(i);
         }
     }
-
-    //进阶解法哈希表
-    /*class Solution {
-        public int[] twoSum(int[] nums, int target) {
-            Map<Integer,Integer> map = new HashMap<>();
-            for(int i=0;i<nums.length;i++){
-                if(map.containsKey(target-nums[i])){
-                    return new int[]{i,map.get(target-nums[i])};
-                }else{
-                    map.put(nums[i],i);
-                }
-            }
-            return null;
-        }
-    }*/
 }
--- src/top/mowangblog/algorithm/easy/day20210830/IsPalindrome.java
+++ src/top/mowangblog/algorithm/easy/day20210830/IsPalindrome.java
@@ -2,30 +2,30 @@ package top.mowangblog.algorithm.easy.day20210830;
 
 /**
  * 给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
- *
+ * <p>
  * 回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。例如，121 是回文，而 123 不是。
- *
+ * <p>
  * 示例 1：
- *
+ * <p>
  * 输入：x = 121
  * 输出：true
  * 示例2：
- *
+ * <p>
  * 输入：x = -121
  * 输出：false
  * 解释：从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
  * 示例 3：
- *
+ * <p>
  * 输入：x = 10
  * 输出：false
  * 解释：从右向左读, 为 01 。因此它不是一个回文数。
  * 示例 4：
- *
+ * <p>
  * 输入：x = -101
  * 输出：false
- *
+ * <p>
  * 提示：
- *
+ * <p>
  * Algorithm-Daily-practice
  * IsPalindrome
  *
@@ -33,31 +33,33 @@ package top.mowangblog.algorithm.easy.day20210830;
  * @date : 2021-08-30 12:11
  */
 public class IsPalindrome {
+    public static void main(String[] args) {
+        new IsPalindrome().isPalindrome(121);
+    }
+
     public boolean isPalindrome(int x) {
-        //基础解法
-        if(x<0){
+        //进阶
+        if (x < 0) {
             return false;
         }
-        String s = ""+x;
-        StringBuffer stringBuffer = new StringBuffer(s);
-        if (stringBuffer.reverse().toString().equals(s)){
-            return true;
+        int result = 0;
+        int num = 0;
+        int original = x;
+        while (x != 0) {
+            num = x % 10;
+            result = 10 * result + num;
+            x /= 10;
         }
-        return false;
-    }
-    //进阶
-//    public boolean isPalindrome(int x) {
+        return result == original;
+        //基础解法
 //        if(x<0){
 //            return false;
 //        }
-//        int a=0,b=0;
-//        int c=x;
-//        while(c!=0){
-//            a=c%10;
-//            b=b*10+a;
-//            c=c/10;
-//
+//        String s = ""+x;
+//        StringBuffer stringBuffer = new StringBuffer(s);
+//        if (stringBuffer.reverse().toString().equals(s)){
+//            return true;
 //        }
-//        return b==x;
-//    }
+//        return false;
+    }
 }
