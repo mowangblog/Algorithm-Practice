15923e2d67b91cec8d0c2441e2eaafaa22c05449
mowangblog@qq.com <mowangblog@qq.com>
2021-09-06 12:50:49 +0800

    add new

---
+++ src/top/mowangblog/algorithm/easy/day20210906/MergeTwoLists.java
@@ -0,0 +1,68 @@
+package top.mowangblog.algorithm.easy.day20210906;
+
+/**
+ *
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ *
+ * 示例 1：
+ *
+ * 输入：l1 = [1,2,4], l2 = [1,3,4]
+ * 输出：[1,1,2,3,4,4]
+ * 示例 2：
+ *
+ * 输入：l1 = [], l2 = []
+ * 输出：[]
+ * 示例 3：
+ *
+ * 输入：l1 = [], l2 = [0]
+ * 输出：[0]
+ * 提示：
+ *
+ * 两个链表的节点数目范围是 [0, 50]
+ * -100 <= Node.val <= 100
+ * l1 和 l2 均按 非递减顺序 排列
+ *
+ * Algorithm-Daily-practice
+ * mergeTwoLists
+ *
+ * @author : Xuan Li
+ * @date : 2021-09-06 12:34
+ **/
+public class MergeTwoLists {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        //我们直接将以上递归过程建模，同时需要考虑边界情况
+        //如果 l1 或者 l2 一开始就是空链表 ，那么没有任何操作需要合并，所以我们只需要返回非空链表。
+        // 否则，我们要判断 l1 和 l2 哪一个链表的头节点的值更小，然后递归地决定下一个添加到结果里的节点
+        // 。如果两个链表有一个为空，递归结束。
+        if(l1 == null){
+            return l2;
+        }
+        if(l2 == null){
+            return l1;
+        }
+        if(l1.val < l2.val){
+            l1.next = mergeTwoLists(l1.next,l2);
+            return l1;
+        }else{
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+
+}
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) {
+          this.val = val;
+      }
+      ListNode(int val, ListNode next) {
+          this.val = val; this.next = next;
+      }
+ }
+
