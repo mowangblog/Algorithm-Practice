c1b475396ae41ae292da7eef9fb86334d5d0dac7
mowangblog@qq.com <mowangblog@qq.com>
2021-11-17 22:51:31 +0800

    ✨Fizz Buzz一行代码

--- src/top/mowangblog/algorithm/easy/day202111/FizzBuzz.java
+++ src/top/mowangblog/algorithm/easy/day202111/FizzBuzz.java
@@ -5,6 +5,32 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 /**
+ * 给你一个整数 n ，找出从 1 到 n 各个整数的 Fizz Buzz 表示，并用字符串数组 answer（下标从 1 开始）返回结果，其中：
+ *
+ * answer[i] == "FizzBuzz" 如果 i 同时是 3 和 5 的倍数。
+ * answer[i] == "Fizz" 如果 i 是 3 的倍数。
+ * answer[i] == "Buzz" 如果 i 是 5 的倍数。
+ * answer[i] == i 如果上述条件全不满足。
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：n = 3
+ * 输出：["1","2","Fizz"]
+ * 示例 2：
+ *
+ * 输入：n = 5
+ * 输出：["1","2","Fizz","4","Buzz"]
+ * 示例 3：
+ *
+ * 输入：n = 15
+ * 输出：["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= n <= 104
+ *
  * Algorithm-Daily-practice
  *
  * @author : Xuan Li<mowangblog@qq.com>
@@ -29,6 +55,8 @@ public class FizzBuzz {
         }
         return answer;
     }
-
+    public List<String> fizzBuzz2(int n) {
+        return IntStream.range(1, n + 1).mapToObj(a -> a % 15 == 0 ? "FizzBuzz" : a % 3 == 0 ? "Fizz" : a % 5 == 0 ? "Buzz" : String.valueOf(a)).collect(Collectors.toList());
+    }
 
 }
