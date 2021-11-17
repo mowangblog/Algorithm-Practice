a04cd66e209fdd42f90153b06abbbbada4b9f956
mowangblog@qq.com <mowangblog@qq.com>
2021-11-17 22:50:41 +0800

    ✨Fizz Buzz

---
+++ src/top/mowangblog/algorithm/easy/day202111/FizzBuzz.java
@@ -0,0 +1,34 @@
+package top.mowangblog.algorithm.easy.day202111;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+/**
+ * Algorithm-Daily-practice
+ *
+ * @author : Xuan Li<mowangblog@qq.com>
+ * @website : https://mowangblog.top
+ * @date : 2021/11/17 22:41
+ **/
+public class FizzBuzz {
+    public List<String> fizzBuzz(int n) {
+        List<String> answer = new ArrayList<String>();
+        for (int i = 1; i <= n; i++) {
+            StringBuffer sb = new StringBuffer();
+            if (i % 3 == 0) {
+                sb.append("Fizz");
+            }
+            if (i % 5 == 0) {
+                sb.append("Buzz");
+            }
+            if (sb.length() == 0) {
+                sb.append(i);
+            }
+            answer.add(sb.toString());
+        }
+        return answer;
+    }
+
+
+}
