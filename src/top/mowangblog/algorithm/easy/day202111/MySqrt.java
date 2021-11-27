cfad57850eb3860a4c704e2fd236c4a150a66814
mowangblog@qq.com <mowangblog@qq.com>
2021-11-27 22:20:15 +0800

    ✨Sqrt(x)

---
+++ src/top/mowangblog/algorithm/easy/day202111/MySqrt.java
@@ -0,0 +1,40 @@
+package top.mowangblog.algorithm.easy.day202111;
+
+/**
+ * 给你一个非负整数 x ，计算并返回 x 的 算术平方根 。
+ *
+ * 由于返回类型是整数，结果只保留 整数部分 ，小数部分将被 舍去 。
+ *
+ * 注意：不允许使用任何内置指数函数和算符，例如 pow(x, 0.5) 或者 x ** 0.5 。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：x = 4
+ * 输出：2
+ * 示例 2：
+ *
+ * 输入：x = 8
+ * 输出：2
+ * 解释：8 的算术平方根是 2.82842..., 由于返回类型是整数，小数部分将被舍去。
+ *  
+ *
+ * 提示：
+ *
+ * 0 <= x <= 231 - 1
+ * Algorithm-Daily-practice
+ *
+ * @author : Xuan Li<mowangblog@qq.com>
+ * @website : https://mowangblog.top
+ * @date : 2021/11/27 22:18
+ **/
+public class MySqrt {
+    public int mySqrt(int x) {
+        if (x == 0) {
+            return 0;
+        }
+        int ans = (int) Math.exp(0.5 * Math.log(x));
+        return (long) (ans + 1) * (ans + 1) <= x ? ans + 1 : ans;
+    }
+}
