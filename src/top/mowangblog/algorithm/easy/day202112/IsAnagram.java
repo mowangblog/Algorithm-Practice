6622a49308c7311f924a104a2b20d10deabd7695
mowangblog@qq.com <mowangblog@qq.com>
2021-12-01 20:45:11 +0800

    ✨有效的字母异位词

---
+++ src/top/mowangblog/algorithm/easy/day202112/IsAnagram.java
@@ -0,0 +1,44 @@
+package top.mowangblog.algorithm.easy.day202112;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+ *
+ * 注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: s = "anagram", t = "nagaram"
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入: s = "rat", t = "car"
+ * 输出: false
+ *  
+ *
+ * 提示:
+ *
+ * 1 <= s.length, t.length <= 5 * 104
+ * s 和 t 仅包含小写字母
+ * Algorithm-Daily-practice
+ *
+ * @author : Xuan Li<mowangblog@qq.com>
+ * @website : https://mowangblog.top
+ * @date : 2021/12/01 20:40
+ **/
+public class IsAnagram {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] str1 = s.toCharArray();
+        char[] str2 = t.toCharArray();
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        return Arrays.equals(str1, str2);
+    }
+}
