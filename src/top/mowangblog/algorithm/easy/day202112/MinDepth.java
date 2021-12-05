244d2a7b5302fb2e0a08f590f4f5bf6d5488ba11
mowangblog@qq.com <mowangblog@qq.com>
2021-12-05 22:40:36 +0800

    ✨二叉树的最小深度

---
+++ src/top/mowangblog/algorithm/easy/day202112/MinDepth.java
@@ -0,0 +1,73 @@
+package top.mowangblog.algorithm.easy.day202112;
+
+
+/**
+ * 给定一个二叉树，找出其最小深度。
+ * <p>
+ * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+ * <p>
+ * 说明：叶子节点是指没有子节点的节点。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：2
+ * 示例 2：
+ * <p>
+ * 输入：root = [2,null,3,null,4,null,5,null,6]
+ * 输出：5
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 树中节点数的范围在 [0, 105] 内
+ * -1000 <= Node.val <= 1000
+ * Algorithm-Daily-practice
+ *
+ * @author : Xuan Li<mowangblog@qq.com>
+ * @website : https://mowangblog.top
+ * @date : 2021/12/05 22:36
+ **/
+public class MinDepth {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+
+        int min_depth = Integer.MAX_VALUE;
+        if (root.left != null) {
+            min_depth = Math.min(minDepth(root.left), min_depth);
+        }
+        if (root.right != null) {
+            min_depth = Math.min(minDepth(root.right), min_depth);
+        }
+
+        return min_depth + 1;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
