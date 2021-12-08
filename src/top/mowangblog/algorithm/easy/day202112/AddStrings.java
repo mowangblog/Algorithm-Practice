3b83aa096bdc0caedd4cf9f48a58f9f2b272c4cc
Xuan Li <lixuan@zszhizhu.com>
2021-12-08 19:12:15 +0800

    字符串相加

---
+++ src/top/mowangblog/algorithm/easy/day202112/AddStrings.java
@@ -0,0 +1,57 @@
+package top.mowangblog.algorithm.easy.day202112;
+
+/**
+ * 给定两个字符串形式的非负整数 num1 和num2 ，计算它们的和并同样以字符串形式返回。
+ *
+ * 你不能使用任何內建的用于处理大整数的库（比如 BigInteger）， 也不能直接将输入的字符串转换为整数形式。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：num1 = "11", num2 = "123"
+ * 输出："134"
+ * 示例 2：
+ *
+ * 输入：num1 = "456", num2 = "77"
+ * 输出："533"
+ * 示例 3：
+ *
+ * 输入：num1 = "0", num2 = "0"
+ * 输出："0"
+ *  
+ *
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= num1.length, num2.length <= 104
+ * num1 和num2 都只包含数字 0-9
+ * num1 和num2 都不包含任何前导零
+ *
+ * Algorithm-Daily-practice
+ *
+ * @author : Xuan Li<mowangblog@qq.com>
+ * @website : https://mowangblog.top
+ * @date : 2021/12/08 13:13
+ **/
+public class AddStrings {
+    class Solution {
+        public String addStrings(String num1, String num2) {
+            int i = num1.length() - 1, j = num2.length() - 1, add = 0;
+            StringBuffer ans = new StringBuffer();
+            while (i >= 0 || j >= 0 || add != 0) {
+                int x = i >= 0 ? num1.charAt(i) - '0' : 0;
+                int y = j >= 0 ? num2.charAt(j) - '0' : 0;
+                int result = x + y + add;
+                ans.append(result % 10);
+                add = result / 10;
+                i--;
+                j--;
+            }
+            // 计算完以后的答案需要翻转过来
+            ans.reverse();
+            return ans.toString();
+        }
+    }
+}
