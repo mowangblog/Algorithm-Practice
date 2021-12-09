fb8eb81983fd1b489a45438e4d0917a24082e6dd
Xuan Li <lixuan@zszhizhu.com>
2021-12-09 17:49:09 +0800

    ✨字符串相加

---
+++ src/top/mowangblog/algorithm/easy/day202112/SumOfLeftLeaves.java
@@ -0,0 +1,30 @@
+package top.mowangblog.algorithm.easy.day202112;
+
+/**
+ * 计算给定二叉树的所有左叶子之和。
+ *
+ * 示例：
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ *
+ * 在这个二叉树中，有两个左叶子，分别是 9 和 15，所以返回 24
+ *
+ * @author : Xuan Li<mowangblog@qq.com>
+ * @website : https://mowangblog.top
+ * @date : 2021/12/09 17:37
+ **/
+public class SumOfLeftLeaves {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if(root==null) return 0;
+        return sumOfLeftLeaves(root.left)
+                + sumOfLeftLeaves(root.right)
+                + (root.left!=null && root.left.left==null && root.left.right==null ? root.left.val : 0);
+    }
+
+
+
+}
