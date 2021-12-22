81d7b6bd11553837c7525c6f3289b3b7dbd211b6
Xuan Li <mowangblog@qq.com>
2021-12-22 22:34:56 +0800

    ✨Excel表列名称

---
+++ src/top/mowangblog/algorithm/easy/day202112/ConvertToTitle.java
@@ -0,0 +1,56 @@
+package top.mowangblog.algorithm.easy.day202112;
+
+/**
+ * 给你一个整数 columnNumber ，返回它在 Excel 表中相对应的列名称。
+ *
+ * 例如：
+ *
+ * A -> 1
+ * B -> 2
+ * C -> 3
+ * ...
+ * Z -> 26
+ * AA -> 27
+ * AB -> 28
+ * ...
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：columnNumber = 1
+ * 输出："A"
+ * 示例 2：
+ *
+ * 输入：columnNumber = 28
+ * 输出："AB"
+ * 示例 3：
+ *
+ * 输入：columnNumber = 701
+ * 输出："ZY"
+ * 示例 4：
+ *
+ * 输入：columnNumber = 2147483647
+ * 输出："FXSHRXW"
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= columnNumber <= 231 - 1
+ *
+ * Algorithm-Practice
+ *
+ * @author : Xuan Li<mowangblog@qq.com>
+ * @website : https://mowangblog.top
+ * @date : 2021/12/22 22:33
+ **/
+public class ConvertToTitle {
+    public String convertToTitle(int columnNumber) {
+        StringBuffer sb = new StringBuffer();
+        while (columnNumber > 0) {
+            int a0 = (columnNumber - 1) % 26 + 1;
+            sb.append((char)(a0 - 1 + 'A'));
+            columnNumber = (columnNumber - a0) / 26;
+        }
+        return sb.reverse().toString();
+    }
+}
