23bd69ea332af2eeb49c5b4134899100fb7f4c94
Xuan Li <mowangblog@qq.com>
2021-12-25 12:09:15 +0800

    ✨区域和检索 - 数组不可变

---
+++ src/top/mowangblog/algorithm/easy/day202112/NumArray.java
@@ -0,0 +1,54 @@
+package top.mowangblog.algorithm.easy.day202112;
+
+/**
+ * 给定一个整数数组  nums，求出数组从索引 i 到 j（i ≤ j）范围内元素的总和，包含 i、j 两点。
+ *
+ * 实现 NumArray 类：
+ *
+ * NumArray(int[] nums) 使用数组 nums 初始化对象
+ * int sumRange(int i, int j) 返回数组 nums 从索引 i 到 j（i ≤ j）范围内元素的总和，包含 i、j 两点（也就是 sum(nums[i], nums[i + 1], ... , nums[j])）
+ *  
+ *
+ * 示例：
+ *
+ * 输入：
+ * ["NumArray", "sumRange", "sumRange", "sumRange"]
+ * [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+ * 输出：
+ * [null, 1, -1, -3]
+ *
+ * 解释：
+ * NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+ * numArray.sumRange(0, 2); // return 1 ((-2) + 0 + 3)
+ * numArray.sumRange(2, 5); // return -1 (3 + (-5) + 2 + (-1))
+ * numArray.sumRange(0, 5); // return -3 ((-2) + 0 + 3 + (-5) + 2 + (-1))
+ *  
+ *
+ * 提示：
+ *
+ * 0 <= nums.length <= 104
+ * -105 <= nums[i] <= 105
+ * 0 <= i <= j < nums.length
+ * 最多调用 104 次 sumRange 方法
+ * Algorithm-Practice
+ *
+ * @author : Xuan Li<mowangblog@qq.com>
+ * @website : https://mowangblog.top
+ * @date : 2021/12/25 12:08
+ **/
+public class NumArray {
+    int[] sums;
+
+    public NumArray(int[] nums) {
+        int n = nums.length;
+        sums = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            sums[i + 1] = sums[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        return sums[j + 1] - sums[i];
+    }
+}
+
