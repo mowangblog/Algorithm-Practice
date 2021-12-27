35ab0e35c3ece56042c0306dcb579d17cbd95336
Xuan Li <mowangblog@qq.com>
2021-12-27 21:51:26 +0800

    ✨数字的补数

---
+++ src/top/mowangblog/algorithm/easy/day202112/FindComplement.java
@@ -0,0 +1,45 @@
+package top.mowangblog.algorithm.easy.day202112;
+
+/**
+ * 对整数的二进制表示取反（0 变 1 ，1 变 0）后，再转换为十进制表示，可以得到这个整数的补数。
+ *
+ * 例如，整数 5 的二进制表示是 "101" ，取反后得到 "010" ，再转回十进制表示得到补数 2 。
+ * 给你一个整数 num ，输出它的补数。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：num = 5
+ * 输出：2
+ * 解释：5 的二进制表示为 101（没有前导零位），其补数为 010。所以你需要输出 2 。
+ * 示例 2：
+ *
+ * 输入：num = 1
+ * 输出：0
+ * 解释：1 的二进制表示为 1（没有前导零位），其补数为 0。所以你需要输出 0 。
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= num < 231
+ * Algorithm-Practice
+ *
+ * @author : Xuan Li<mowangblog@qq.com>
+ * @website : https://mowangblog.top
+ * @date : 2021/12/27 21:49
+ **/
+public class FindComplement {
+    public int findComplement(int num) {
+        int highbit = 0;
+        for (int i = 1; i <= 30; ++i) {
+            if (num >= 1 << i) {
+                highbit = i;
+            } else {
+                break;
+            }
+        }
+        int mask = highbit == 30 ? 0x7fffffff : (1 << (highbit + 1)) - 1;
+        return num ^ mask;
+    }
+}
