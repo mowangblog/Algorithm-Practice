803a556f3e7d0b7f0f667706e702960754b11c5e
Xuan Li <mowangblog@qq.com>
2022-01-11 23:25:46 +0800

    ✨大连续 1 的个数

---
+++ src/top/mowangblog/algorithm/easy/day202201/FindMaxConsecutiveOnes.java
@@ -0,0 +1,42 @@
+package top.mowangblog.algorithm.easy.day202201;
+
+/**
+ *
+ * 给定一个二进制数组， 计算其中最大连续 1 的个数。
+ *
+ *  
+ *
+ * 示例：
+ *
+ * 输入：[1,1,0,1,1,1]
+ * 输出：3
+ * 解释：开头的两位和最后的三位都是连续 1 ，所以最大连续 1 的个数是 3.
+ *  
+ *
+ * 提示：
+ *
+ * 输入的数组只包含 0 和 1 。
+ * 输入数组的长度是正整数，且不超过 10,000。
+ *
+ * Algorithm-Practice
+ *
+ * @author : Xuan Li<mowangblog@qq.com>
+ * @website : https://mowangblog.top
+ * @date : 2022/01/11 23:24
+ **/
+public class FindMaxConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int maxCount = 0, count = 0;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 1) {
+                count++;
+            } else {
+                maxCount = Math.max(maxCount, count);
+                count = 0;
+            }
+        }
+        maxCount = Math.max(maxCount, count);
+        return maxCount;
+    }
+}
