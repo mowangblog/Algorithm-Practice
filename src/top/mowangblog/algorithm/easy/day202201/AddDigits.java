4a0e876141663cb5925ec6d1dbb8c56ae7a1a588
Xuan Li <mowangblog@qq.com>
2022-01-16 21:06:42 +0800

    ✨各位相加

---
+++ src/top/mowangblog/algorithm/easy/day202201/AddDigits.java
@@ -0,0 +1,24 @@
+package top.mowangblog.algorithm.easy.day202201;
+
+/**
+ *
+ * 给定一个非负整数 num，反复将各个位上的数字相加，直到结果为一位数。
+ *
+ * 示例:
+ *
+ * 输入: 38
+ * 输出: 2
+ * 解释: 各位相加的过程为：3 + 8 = 11, 1 + 1 = 2。 由于 2 是一位数，所以返回 2。
+ * 进阶:
+ * 你可以不使用循环或者递归，且在 O(1) 时间复杂度内解决这个问题吗？
+ * Algorithm-Practice
+ *
+ * @author : Xuan Li<mowangblog@qq.com>
+ * @website : https://mowangblog.top
+ * @date : 2022/01/16 21:05
+ **/
+public class AddDigits {
+    public int addDigits(int num) {
+        return (num - 1) % 9 + 1;
+    }
+}
