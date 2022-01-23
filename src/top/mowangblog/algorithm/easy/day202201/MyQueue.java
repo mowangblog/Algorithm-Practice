2ef88a4015f37863c58eff2eed213373f89e9386
Xuan Li <mowangblog@qq.com>
2022-01-23 21:15:35 +0800

    ✨用栈实现队列

---
+++ src/top/mowangblog/algorithm/easy/day202201/MyQueue.java
@@ -0,0 +1,90 @@
+package top.mowangblog.algorithm.easy.day202201;
+
+import java.util.Stack;
+
+/**
+ * 请你仅使用两个栈实现先入先出队列。队列应当支持一般队列支持的所有操作（push、pop、peek、empty）：
+ *
+ * 实现 MyQueue 类：
+ *
+ * void push(int x) 将元素 x 推到队列的末尾
+ * int pop() 从队列的开头移除并返回元素
+ * int peek() 返回队列开头的元素
+ * boolean empty() 如果队列为空，返回 true ；否则，返回 false
+ *  
+ *
+ * 说明：
+ *
+ * 你只能使用标准的栈操作 —— 也就是只有 push to top, peek/pop from top, size, 和 is empty 操作是合法的。
+ * 你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
+ *  
+ *
+ * 进阶：
+ *
+ * 你能否实现每个操作均摊时间复杂度为 O(1) 的队列？换句话说，执行 n 个操作的总时间复杂度为 O(n) ，即使其中一个操作可能花费较长时间。
+ *  
+ *
+ * 示例：
+ *
+ * 输入：
+ * ["MyQueue", "push", "push", "peek", "pop", "empty"]
+ * [[], [1], [2], [], [], []]
+ * 输出：
+ * [null, null, null, 1, 1, false]
+ *
+ * 解释：
+ * MyQueue myQueue = new MyQueue();
+ * myQueue.push(1); // queue is: [1]
+ * myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
+ * myQueue.peek(); // return 1
+ * myQueue.pop(); // return 1, queue is [2]
+ * myQueue.empty(); // return false
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= x <= 9
+ * 最多调用 100 次 push、pop、peek 和 empty
+ * 假设所有操作都是有效的 （例如，一个空的队列不会调用 pop 或者 peek 操作）
+ * Algorithm-Practice
+ *
+ * @author : Xuan Li<mowangblog@qq.com>
+ * @website : https://mowangblog.top
+ * @date : 2022/01/23 21:13
+ **/
+public class MyQueue {
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+
+    public MyQueue() {
+        this.stack1 = new Stack<>();
+        this.stack2 = new Stack<>();
+    }
+
+    public void push(int x) {
+        this.stack1.push(x);
+    }
+
+    public int pop() {
+        while(stack2.empty()) {
+            while(!stack1.empty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
+    public int peek() {
+        while(stack2.empty()) {
+            while(!stack1.empty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
+    }
+
+    public boolean empty() {
+        //两个栈都为空才返回
+        return this.stack1.empty() && this.stack2.empty();
+    }
+}
