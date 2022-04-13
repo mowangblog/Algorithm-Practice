1e80b91d8bf3be44c376857f38a5748fd465a534
Xuan Li <lixuan@zszhizhu.com>
2022-04-13 18:42:08 +0800

    fix:路径总和

---
+++ src/top/mowangblog/algorithm/easy/day202112/TreeNode.java
@@ -0,0 +1,27 @@
+package top.mowangblog.algorithm.easy.day202112;
+
+/**
+ * Algorithm-Practice
+ *
+ * @author : Xuan Li<mowangblog@qq.com>
+ * @website : https://mowangblog.top
+ * @date : 2022/04/13 18:29
+ **/
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
